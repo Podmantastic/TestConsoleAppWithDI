cb94efd08c5dc049c695cc28cec9c6ee65eb610f
LloydA <LloydA@DESKTOP-2SIRC6H>
2025-05-15 13:13:43 -0700

    Unit test was failing when asked to get the index of element is last position in array

--- TestConsoleAppWithDI/ConsoleAppEntryPoint.Partial.cs
+++ TestConsoleAppWithDI/ConsoleAppEntryPoint.Partial.cs
@@ -12,25 +12,25 @@ public partial class ConsoleAppEntryPoint
         int N = A.Length;
         if (N == 0)
         {
-            return (-1);
+            return -1;
         }
         int l = 0;
         int r = N - 1;
-        while (l < r)
+        while (l <= r)
         {
             int m = (l + r) / 2;
-            if (A[m] > X)
+            if (A[m] == X)
+            {
+                return m;
+            }
+            else if (A[m] > X)
             {
                 r = m - 1;
             }
             else
             {
-                l = m;
+                l = m + 1;
             }
-        }
-        if (A[l] == X)
-        {
-            return l;
         }
         return -1;
     }
